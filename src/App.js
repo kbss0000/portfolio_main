8b355d36999aa7b68f0e214942cb368e51b12812
kbssrikar7 <kbsivasrikar@gmail.com>
2025-12-01 22:57:36 +0530

    Add GitHub Actions workflow for automatic deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import Projects from './pages/Projects';
 import Experience from './pages/Experience';
 import MainPage from './pages/MainPage';
 
-const App = () => (
-    <Router>
-        <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/experience" element={<Experience />} />
-            <Route path="/" element={<MainPage />} />
-        </Routes>
-    </Router>
-);
+const App = () => {
+    // Get basename from package.json homepage or use empty string for local dev
+    const basename = process.env.PUBLIC_URL ? new URL(process.env.PUBLIC_URL).pathname : '';
+    
+    return (
+        <Router basename={basename}>
+            <Routes>
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/experience" element={<Experience />} />
+                <Route path="/" element={<MainPage />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
